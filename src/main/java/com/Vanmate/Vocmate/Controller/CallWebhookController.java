d98157df331084e9829d7992541384b6e5b8b0d5
Bonagiri Dilipkumar <bonagiridilipkumar970@gmail.com>
2025-07-22 01:50:03 +0530

    Updated the maven Dependency

--- src/main/java/com/Vanmate/Vocmate/Controller/CallWebhookController.java
+++ src/main/java/com/Vanmate/Vocmate/Controller/CallWebhookController.java
@@ -15,10 +15,19 @@ public class CallWebhookController {
     @PostMapping ("/incoming call")
     public ResponseEntity<String> handleIncomingCall() {
         // TwiML response to speak something to the caller
+        // This method will run whenever a POST request is sent to /api/calls/webhook
+        // Twilio will send a POST request here when someone calls your Twilio number
         String twimlResponse=
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + // XMl Declaration for SpringBoot
                         "<Response>" +
                         "<Say voice=\"alice\" language=\"en-US\">Hello! Your call is connected to VocMate test webhook. Thank you!</Say>" +
                         "<Response>";
+
+        // the HTTP response with a 200 OK status
+        // Content-Type header to application/xml (because Twilio expects XML)
+        // Sending back the TwiML as the body
+        return ResponseEntity.ok()
+                .header("Content-Type", "application/xml")
+                .body(twimlResponse);
     }
 }
